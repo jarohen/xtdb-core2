a5e672d400a1940139007b42ec2b332ac4d7171f
James Henderson <jms@juxt.pro>
2022-09-14 16:50:03 +0100

    snaking the col-names in the external data test s.t. they can be used from the SLT test

--- test/core2/operator/external_data_test.clj
+++ test/core2/operator/external_data_test.clj
@@ -6,27 +6,28 @@
             [core2.util :as util])
   (:import org.apache.arrow.memory.RootAllocator
            org.apache.arrow.vector.types.pojo.Schema
-           org.apache.arrow.vector.VectorSchemaRoot))
+           org.apache.arrow.vector.VectorSchemaRoot
+           java.net.URL))
 
 (t/use-fixtures :once tu/with-allocator)
 
 (def example-data
-  [[{:id "foo1", :a-long 10, :a-double 54.2, :an-inst (util/->zdt #inst "2021")}
-    {:id "foo2", :a-long 82, :a-double 1052.25, :an-inst (util/->zdt #inst "2021-01-04")}
-    {:id "foo3", :a-long -15, :a-double -1534.23, :an-inst (util/->zdt #inst "2021-01-04T12:13")}]
-   [{:id "foo4", :a-long 0, :a-double 0.0, :an-inst (util/->zdt #inst "2021-05-21T17:30")}
-    {:id "foo5", :a-long 53, :a-double 10.0, :an-inst (util/->zdt #inst "2022")}]])
+  [[{:id "foo1", :a_long 10, :a_double 54.2, :an_inst (util/->zdt #inst "2021")}
+    {:id "foo2", :a_long 82, :a_double 1052.25, :an_inst (util/->zdt #inst "2021-01-04")}
+    {:id "foo3", :a_long -15, :a_double -1534.23, :an_inst (util/->zdt #inst "2021-01-04T12:13")}]
+   [{:id "foo4", :a_long 0, :a_double 0.0, :an_inst (util/->zdt #inst "2021-05-21T17:30")}
+    {:id "foo5", :a_long 53, :a_double 10.0, :an_inst (util/->zdt #inst "2022")}]])
 
 (t/deftest test-csv-cursor
-  (t/is (= {:col-types {"id" :utf8, "a-long" :i64, "a-double" :f64, "an-inst" [:timestamp-tz :micro "UTC"]}
+  (t/is (= {:col-types {"id" :utf8, "a_long" :i64, "a_double" :f64, "an_inst" [:timestamp-tz :micro "UTC"]}
             :res example-data}
            (tu/query-ra [:csv (-> (io/resource "core2/operator/csv-cursor-test.csv")
                                   .toURI
                                   util/->path)
                          '{id :utf8
-                           a-long :i64
-                           a-double :f64
-                           an-inst :timestamp}
+                           a_long :i64
+                           a_double :f64
+                           an_inst :timestamp}
                          {:batch-size 3}]
                         {:preserve-blocks? true
                          :with-col-types? true}))))
@@ -38,7 +39,7 @@
   (io/resource "core2/operator/arrow-cursor-test.arrow"))
 
 (t/deftest test-arrow-cursor
-  (let [expected {:col-types '{id :utf8, a-long :i64, a-double :f64, an-inst [:timestamp-tz :micro "UTC"]}
+  (let [expected {:col-types '{id :utf8, a_long :i64, a_double :f64, an_inst [:timestamp-tz :micro "UTC"]}
                   :res example-data}]
     (t/is (= expected (tu/query-ra [:arrow arrow-file-url]
                                    {:preserve-blocks? true, :with-col-types? true})))
@@ -46,18 +47,18 @@
                                    {:preserve-blocks? true, :with-col-types? true})))))
 
 (comment
-  (let  [arrow-path (-> (io/resource "core2/operator/arrow-cursor-test.arrows")
-                        .toURI
-                        util/->path)]
-    (with-open [al (RootAllocator.)
-                root (VectorSchemaRoot/create (Schema. [(types/col-type->field "id" :utf8)
-                                                        (types/col-type->field "a-long" :i64)
-                                                        (types/col-type->field "a-double" :f64)
-                                                        (types/col-type->field "an-inst" [:timestamp-tz :micro "UTC"])])
-                                              al)]
-      (doto (util/build-arrow-ipc-byte-buffer root :stream
-              (fn [write-batch!]
-                (doseq [block example-data]
-                  (tu/populate-root root block)
-                  (write-batch!))))
-        (util/write-buffer-to-path arrow-path)))))
+  (doseq [[^URL url arrow-mode] [[arrow-stream-url :stream]
+                                 [arrow-file-url :file]]]
+    (let [arrow-path (-> (.toURI url) (util/->path))]
+      (with-open [al (RootAllocator.)
+                  root (VectorSchemaRoot/create (Schema. [(types/col-type->field "id" :utf8)
+                                                          (types/col-type->field "a_long" :i64)
+                                                          (types/col-type->field "a_double" :f64)
+                                                          (types/col-type->field "an_inst" [:timestamp-tz :micro "UTC"])])
+                                                al)]
+        (doto (util/build-arrow-ipc-byte-buffer root arrow-mode
+                (fn [write-batch!]
+                  (doseq [block example-data]
+                    (tu/populate-root root block)
+                    (write-batch!))))
+          (util/write-buffer-to-path arrow-path))))))
