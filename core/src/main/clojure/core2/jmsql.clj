a4c622447f14b732e52144b2b7f9264a1c6efe03
James Henderson <james@jarohen.dev>
2023-02-19 08:15:53 +0000

    add group to jmsql [ci skip]

--- core/src/main/clojure/core2/jmsql.clj
+++ core/src/main/clojure/core2/jmsql.clj
@@ -94,21 +94,30 @@
          :top-params (s/keys :opt-un [::limit ::offset])
          :query ::query))
 
-(s/def ::order-by-value ::column)
+(s/def ::order-by-value ::form)
 
-(s/def ::order-by-spec
+(s/def ::order-spec
   (s/and (s/or :value ::order-by-value
                :value+direction (s/cat :value ::order-by-value
                                        :direction #{:asc :desc}))
-         (s/conformer (fn [[obs-tag obs-arg]]
-                        (case obs-tag
-                          :value {:value obs-arg, :direction :asc}
-                          :value+direction obs-arg))
+         (s/conformer (fn [[tag arg]]
+                        (case tag
+                          :value {:value arg, :direction :asc}
+                          :value+direction arg))
                       (fn [v] [:value+direction v]))))
 
-(defmethod query-spec :order-by [_]
-  (s/cat :op #{:order-by}
-         :order-by-specs (s/coll-of ::order-by-spec, :kind vector?)
+(defmethod query-spec :order [_]
+  (s/cat :op #{:order}
+         :order-specs (s/coll-of ::order-spec, :kind vector?)
+         :query ::query))
+
+(s/def ::group-spec
+  (s/or :named (s/map-of ::column ::form, :conform-keys true)
+        :form ::form))
+
+(defmethod query-spec :group [_]
+  (s/cat :op #{:group}
+         :group-specs (s/coll-of ::group-spec, :kind vector?)
          :query ::query))
 
 (defn- conform-query [query]
@@ -157,7 +166,7 @@
                              [:foreach plan]]))))
         var->cols (-> (for [[qid [_ plan]] qs
                             box-var (qgm/box-vars plan)]
-                        (MapEntry/create box-var [:column [qid box-var]]))
+                        (MapEntry/create box-var [:q-column qid box-var]))
                       (->> (group-by key))
                       (update-vals vals))]
 
@@ -174,6 +183,9 @@
                       (into [:call f] (mapv unform* args)))))]
     (unform* form)))
 
+(defn- unq-var->col [lv] [:column lv])
+(defn- single-q-var->col [lv] [:q-column 'q lv])
+
 (defmethod plan-query :where [{:keys [clauses query]}]
   (let [plan (plan-query query)]
     (if (= :select (first plan))
@@ -185,26 +197,22 @@
          qs])
 
       [:select (->> (qgm/box-vars plan)
-                    (into {} (map (juxt identity
-                                        (fn [lv]
-                                          [:column 'q lv])))))
-       (->> clauses (mapv (partial unform-form (constantly 'q))))
+                    (into {} (map (juxt identity single-q-var->col))))
+       (->> clauses (mapv (partial unform-form single-q-var->col)))
        {'q plan}])))
 
 (defmethod plan-query :project [{:keys [clauses query]}]
   (let [plan (plan-query query)
         var->col (->> (qgm/box-vars plan)
-                      (into {} (map (juxt identity
-                                          (fn [lv]
-                                            [:column 'q lv])))))
+                      (into {} (map (juxt identity single-q-var->col))))
         projections (->> clauses
                          (reduce
                           (fn [acc [p-tag p-arg]]
                             (case p-tag
                               :star (into acc
-                                          (map (juxt identity (fn [lv] [:column 'q lv])))
+                                          (map (juxt identity var->col))
                                           (qgm/box-vars plan))
-                              :column (-> acc (assoc p-arg [:column 'q p-arg]))
+                              :column (-> acc (assoc p-arg (single-q-var->col p-arg)))
                               :extends (into acc
                                              (map (fn [[out-col form]]
                                                     [out-col [:extends (unform-form var->col form)]]))
@@ -217,18 +225,27 @@
 (defmethod plan-query :top [{:keys [top-params query]}]
   [:top top-params (plan-query query)])
 
-(defmethod plan-query :order-by [{:keys [order-by-specs query]}]
-  [:order-by (mapv (juxt :value :direction) order-by-specs)
+(defmethod plan-query :order [{:keys [order-specs query]}]
+  [:order (mapv (juxt (comp (partial unform-form unq-var->col) :value) :direction) order-specs)
+   (plan-query query)])
+
+(defmethod plan-query :group [{:keys [group-specs query]}]
+  [:group (->> group-specs
+               (mapv (fn [[tag arg]]
+                       (case tag
+                         :form [:form (unform-form unq-var->col arg)]
+                         :named [:named (-> arg (update-vals (partial unform-form unq-var->col)))]))))
    (plan-query query)])
 
 (comment
-  (compile-query '[:order-by [id]
+  (compile-query '[:order [id]
                    [:top {:limit 10}
-                    [:project [* id {id2 (* id 2)}]
-                     [:where [(> id 4)
-                              (< uid 2)]
-                      [:match [(users [uid])
-                               (products [id])]]]]]]))
+                    [:group [id {c (count id2)}]
+                     [:project [* id {id2 (* id 2)}]
+                      [:where [(> id 4)
+                               (< uid 2)]
+                       [:match [(users [uid])
+                                (products [id])]]]]]]]))
 
 (defn compile-query [query]
   (-> (conform-query query)
